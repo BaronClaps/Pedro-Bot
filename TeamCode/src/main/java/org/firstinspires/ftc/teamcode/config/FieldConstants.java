420cc7c76a1e809b2b219959b4043a6540407eaa
BaronCLaps <barbarhendo180@gmail.com>
2024-07-31 08:26:26 -0400

    yay

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/FieldConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/FieldConstants.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.config;
 import org.firstinspires.ftc.teamcode.config.pedroPathing.localization.Pose;
 
 public class FieldConstants {
-    public static final double sStart = 0.5;
-    public static final Pose BlueCloseLeftSpike = new Pose();
+    public static final Pose BlueCloseLeftPurple = new Pose();
+    public static final Pose BlueCloseLeftYellow = new Pose();
+
 }
